b1baa29429052f2901ff97abd9de11a746859a12
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-07-01 00:22:07 -0300

    Schema para o signUp

---
+++ src/schemas/signUpSchema.js
@@ -0,0 +1,9 @@
+import joi from 'joi';
+
+const signUpSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required().email(),
+    password: joi.string().required(),
+});
+
+export default signUpSchema;
