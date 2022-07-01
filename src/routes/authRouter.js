e4fe00db9b524fa05a4037d8fbbd444b7eafa998
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-07-01 12:54:25 -0300

    feat: add signUp router

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
 import { validateSignIn } from '../middlewares/validateSignInMiddleware.js';
+import { validateSignUn } from '../middlewares/validadeSignUpMiddleware.js';
 import { signIn } from '../controllers/authController.js'; 
+import { singUp } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.get('/signIn', validateSignIn, signIn);
+router.post('/signUp', validateSignUn, singUp);
 
 export default router;
