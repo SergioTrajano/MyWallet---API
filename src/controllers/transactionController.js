576f19ffe252f33ea9d84638d249417a5f5eee72
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-07-02 17:03:46 -0300

    feat: add controller to route transaction to update db on push requests

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -7,3 +7,20 @@ export async function getTransactionHistory(req, res) {
 
     res.send(userTransaction.transactions);
 }
+
+export async function postOnTransactionHistory(req, res) {
+    const sessionId = res.locals.session.userId;
+    const newTransaction = req.body;
+
+    try {
+        await db.collection('transactionHistory').updateOne(
+            { id: new ObjectId(sessionId) },
+            { $push: { transactions: newTransaction} }
+        );
+
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
